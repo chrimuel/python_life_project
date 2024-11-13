9dac1d8e2d154304f0ff3b6940d17e77d20c0c18
christoph <dev@muc.ch>
2024-11-13 11:35:51 +0100

    first commit

---
+++ 01_maze/maze.py
@@ -0,0 +1,14 @@
+# Define the maze as a string with newlines for row separation
+maze_str = """
+##########
+#S   #   #
+# ## # # #
+#    #   #
+###### ###
+#      E #
+##########
+"""
+
+# Print the maze
+print("Maze:")
+print(maze_str)
